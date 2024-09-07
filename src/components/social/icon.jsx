d8430c09764522be1c8c595b46c0f533481acc60
subhibhat <s_subhibhat@hotmail.com>
2024-09-07 09:40:09 +0700

    Updates

--- src/components/social/icon.jsx
+++
@@ -1,8 +0,0 @@
-// import 'font-awesome/scss/font-awesome.scss'
-import '@fortawesome/fontawesome-free/scss/fontawesome.scss'
-import '@fortawesome/fontawesome-free/scss/brands.scss'
-
-export const Icon = ({name, spin=false, className=''}) => {
-    const classText = `fa-brands fa-${name} ${spin ? 'fa-spin' : ''} ${className ? className : ''}`
-    return(<i className={classText.trim()} />)
-}
